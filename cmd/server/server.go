644f848fbb0d1a041da9b68621d205dca1a3ec1b
Akshay Kolli <proflepton@gmail.com>
2022-09-30 12:59:03 -0400

    Server code v1
    Added server code.

---
+++ cmd/server/server.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+// Initializing VARS
+var (
+	DIFFICULTY    = 6
+	FILE_NAME     = "./blockChain.txt"
+	GENESIS_BLOCK = "0000000000000000000000000000000000000000000000000000000000000000~satoshi~11970128322"
+	TAIL          = GENESIS_BLOCK
+	TERMINATOR    = '\n'
+	ADDRESS       = ":8888"
+)
+
+func main() {
+
+	log.Println("Server> Starting the server.")
+
+	//Check and create BlockChain File
+	TAIL = initializeServer(FILE_NAME)
+	log.Printf("TAIL BLOCK: %s\n", TAIL)
+
+	//Start server and handle connections
+	log.Println("Server> Listening for connections.")
+	serverListener, err := net.Listen("tcp", ADDRESS)
+	if err != nil {
+		panic(err)
+	}
+
+	//Run and look for connections.
+	for {
+		serverConnection, err := serverListener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go HandleConnections(serverConnection)
+	}
+
+}
+
+// Block is defined as the {hash of previous block}~{hash of name}~{name}~{nonce}
+// Store the entire block chain into a file
+// Each block has the hash of the previous b
+// When the server receives a block, it checks if the hash of the block string
+// \n satisfies the given difficulty rating.
+// The difficulty rating is constant
+// The server needs to be able to connect to multiple nodes asynchronously,
+// It needs to be able to verify 2 things, namely the 1) If the block string contains the
+// hash of the current blockchain tail, second if the hash of the block string satisfies the POW requirement.
+// Two functions needed HashString, veriifyPOW
+// Need to story the current block tip
+// Also need to give entire block chain to all  nodes when requested.
+// Need read write permissions to file.
+// Need to update the file and blockchain tip when new block is accepted.
+// Need to convert hash to hexa
+
+// Returns the Hex of a Hash as a string.
+func HexaHashFromString(s string) string {
+	hash := sha256.Sum256([]byte(s))
+	return fmt.Sprintf("%064x", hash)
+}
+
+// Handles connections from all clients
+// Recognize different requests - Block requests & Submit requests
+// Be able to read and answer block requests
+// Be able to verify and save submit requests,
+func HandleConnections(conn net.Conn) {
+	log.Printf("Server> Connected to %s\n", conn.RemoteAddr().String())
+
+	//Read incoming connections
+	bufReader := bufio.NewReader(conn)
+
+	recvdBytes, err := bufReader.ReadBytes('\n')
+	if err != nil {
+		log.Printf("TCP error: %s\n", err.Error())
+	}
+	recvdString := string(recvdBytes)
+	var sendBytes []byte
+	var sendString string
+	if strings.HasPrefix(recvdString, "TAIL") {
+		sendString = TAIL
+	} else {
+		newBl := recvdString
+		if VerifyBlockString(TAIL, newBl) {
+			//Block has been accepted.
+			TAIL = newBl
+			appendBlockToDisk(newBl, FILE_NAME)
+			sendString = "BLOCK ACCEPTED"
+		} else {
+			sendString = "Invalid Submission"
+		}
+	}
+
+	//Sending stuff
+	sendBytes = []byte(fmt.Sprintf("%s\n", sendString))
+	_, err = conn.Write(sendBytes)
+	if err != nil {
+		log.Printf("TCP error: %s\n", err.Error())
+	}
+
+	//Disconnect
+	conn.Close()
+}
+
+// Takes in a string and a difficulty int and checks if the POW is satisfied.
+func checkPOW(s string, difficulty int) bool {
+	zeroes := 0
+	for i := range make([]int, difficulty) {
+		if s[i] == '0' {
+			zeroes++
+		}
+	}
+	return zeroes == difficulty
+}
+
+// Verifies if the submitted block is a valid solution.
+func VerifyBlockString(TAIL string, submittedBlock string) bool {
+	bctHash := HexaHashFromString(TAIL)
+	block_array := strings.Split(submittedBlock, "~")
+
+	//Check if block has this bctHash at the correct spot, if yes then check pow
+
+	//else return false if pow is not satisfied or hctHash is misplaced or incorrect.
+
+	if bctHash == block_array[0] {
+		//check proof of work
+		submittedBlockHash := HexaHashFromString(submittedBlock)
+		return checkPOW(submittedBlockHash, DIFFICULTY)
+
+	}
+	return false
+}
+
+func initializeServer(file_name string) string {
+	f, err := os.Open(file_name)
+	if err != nil {
+		f, err = os.Create(file_name)
+		if err != nil {
+			panic(err)
+		}
+		f.WriteString(TAIL + "\n")
+		return TAIL
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	var block string
+
+	for scanner.Scan() {
+		block = scanner.Text()
+	}
+
+	return block
+}
+
+func appendBlockToDisk(block string, file_name string) {
+	f, err := os.Open(FILE_NAME)
+	if err != nil {
+		panic(err)
+	}
+	f.WriteString(block + "\n")
+	f.Close()
+}
